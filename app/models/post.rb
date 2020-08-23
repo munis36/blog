17e2b1d33c7e0abf9912c8c079725455b0c0f792
mnagarat <munis36@hotmail.com>
2020-08-23 07:00:48 -0400

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,8 @@
 #updated_at :datetime   not null
 
 class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
+    validates_presence_of :title
+    validates_presence_of :body
+    belongs_to :user
 end
